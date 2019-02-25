fa528372e38ddcb1f96140866b1c7a3eaa94fa24
herndont <herndontrey@gmail.com>
2019-02-24 22:15:48 -0600

    button update

--- app.js
+++ app.js
@@ -29,28 +29,6 @@ var Product = function(name, url){
   product_catalogue.push(this);
 };
 
-//Creating the products into objects
-// new Product ('bag', './img/bag.jpg');
-// new Product ('banana', './img/banana.jpg');
-// new Product ('bathroom', './img/bathroom.jpg');
-// new Product ('boots', './img/boots.jpg');
-// new Product ('breakfast', './img/breakfast.jpg');
-// new Product ('bubblegum', './img/bubblegum.jpg');
-// new Product ('chair', './img/chair.jpg');
-// new Product ('cthulhu', './img/cthulhu.jpg');
-// new Product ('dog-duck', './img/dog-duck.jpg');
-// new Product ('dragon', './img/dragon.jpg');
-// new Product ('pen', './img/pen.jpg');
-// new Product ('pet-sweep', './img/pet-sweep.jpg');
-// new Product ('scissors', './img/scissors.jpg');
-// new Product ('shark', './img/shark.jpg');
-// new Product ('sweep', './img/sweep.png');
-// new Product ('tauntaun', './img/tauntaun.jpg');
-// new Product ('unicorn', './img/unicorn.jpg');
-// new Product ('usb', './img/usb.gif');
-// new Product ('water-can', './img/water-can.jpg');
-// new Product ('wine-glass', './img/wine-glass.jpg');
-
 function chart_image(){
   var product_click_data = [];
   for(var j = 0; j < product_catalogue.length; j++){
